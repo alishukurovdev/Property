a3960f9a82dad43df21bd4ff485140301d49e7d0
ALIWKA-44_ <sukurovali17@gmail.com>
2024-04-02 23:06:04 +0400

    silah atisi

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System.IO;
+
+namespace Property
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isContinue = true;
+            Weapon weapon= new Weapon(15,15,3,"Automatic");
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(@"0 - İnformasiya almaq üçün
+1 - Shoot metodu üçün
+2 - Fire metodu üçün
+3 - GetRemainBulletCount metodu üçün
+4 - Reload metodu üçün
+5 - ChangeFireMode metodu üçün
+6 - Proqramdan dayandırmaq üçün
+qısayoldur.
+7 - Redakte etmek üçün");
+                int input = Convert.ToInt32(Console.ReadLine());
+                Console.ReadKey();
+                switch (input)
+                {
+                    case 0:
+                        Console.OutputEncoding = System.Text.Encoding.UTF8;
+                        Console.WriteLine("Xoş gəlmisiz.\n" +
+                            "Bura da siz seçdiyiniz rəqəmə görə silahın güllə tutumu, güllə sayı, boşalma saniysi və atış modun görə bilərsiz.");
+                        Console.ReadKey();
+                        break;
+                    case 1:
+                        weapon.Shoot();
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        weapon.Fire();
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        weapon.GetRemainBulletCount();
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        weapon.Reload();
+                        Console.ReadKey();
+                        break;
+                    case 5:
+                        weapon.ChangeFireMode();
+                            Console.ReadKey();
+                        break;
+                    case 6:
+                        isContinue = false;
+                        break;
+
+                    case 7:
+                        Console.WriteLine("Enter option (T - Weapon Mode, S - Bullet Count, D - Reset Second):");
+                        string option = Console.ReadLine();
+                        Console.ReadKey();
+                        switch (option)
+                        {
+                            case "T":
+                                Console.WriteLine("Yeni silah daxil edin:");
+                                weapon.WeaponMode = Console.ReadLine();
+                                break;
+                            case "S":
+                                Console.WriteLine("Gulle sayi daxil edin:");
+                                weapon.BulletCount = Convert.ToInt32(Console.ReadLine());
+                                break;
+                            case "D":
+                                Console.WriteLine("Sifirlama saniyesin daxil edin:");
+                                weapon.FinishSeconds = Convert.ToDouble(Console.ReadLine());
+                                break;
+                            default:
+                                Console.WriteLine("Yalnis secdiniz!");
+                                break;
+                        }
+                        break;
+
+                }
+            } while (isContinue);
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property
+{
+    internal class Weapon
+    {
+        private int _bulletCapacity;  // gulle tutmu
+        public int _bulletCount; // gulle sayi
+        private double _finishSeconds;  // bosalma saniyesi
+        private string _weaponMode;  // atis modu
+
+        public int BulletCapacity
+        {
+            get
+            {
+                return _bulletCapacity;
+            }
+            set
+            {
+                _bulletCapacity = value;
+            }
+        }
+        public int BulletCount
+        {
+            get
+            {
+                return _bulletCount;
+            }
+            set
+            {
+                _bulletCount = value;
+            }
+        }
+        public double FinishSeconds
+        {
+            get
+            {
+                return _finishSeconds;
+            }
+            set
+            {
+                _finishSeconds = value;
+            }
+        }
+        public string WeaponMode
+        {
+            get
+            {
+                return _weaponMode;
+            }
+            set
+            {
+                _weaponMode = value;
+            }
+        }
+        public Weapon(int bulletCapacity, int bulletCount, double finishSeconds, string weaponMode)
+        {
+            BulletCapacity = bulletCapacity;
+            BulletCount = bulletCount;
+            FinishSeconds = finishSeconds;
+            WeaponMode = weaponMode;
+
+        }
+
+        public void Shoot()
+        {
+            if (BulletCount > 0)
+            {
+                Console.WriteLine(" tvu tvuu  part part");
+                BulletCount--;
+            }
+            else
+            {
+                Console.WriteLine("Gulle bitib zehmet olmasa yeniden doldurun");
+            }
+
+        }    
+        public void Fire()
+        {
+            Console.WriteLine($"{_bulletCount} gulle atildi, serf olunan vaxt:{_bulletCount*_finishSeconds}" );
+            _bulletCount = 0;
+        }
+
+        public void GetRemainBulletCount()
+        {//--------------------*
+            Console.WriteLine($"Dolması üçün lazım olan gülle: {_bulletCapacity - _bulletCount}");
+        }
+        public void Reload()
+        { 
+            BulletCount= BulletCapacity;
+            Console.WriteLine($"Gulleniz dolduruldu: {BulletCount}");
+        }
+        public void ChangeFireMode()
+        {
+            if (_weaponMode == "Automatic")
+            {
+                _weaponMode = "Automatic";
+            }
+            else
+            {
+                _weaponMode = "Single";
+            }
+
+            Console.WriteLine($"Atis modu deyisildi {_weaponMode}");
+        }
+
+    }
+}
+
